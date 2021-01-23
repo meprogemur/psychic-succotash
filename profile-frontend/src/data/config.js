04c9e8981bb8a4208f3bcfebba6ac50393043828
Binit Agarwal <rockingbinitagarwal@gmail.com>
2021-01-23 23:05:53 +0530

    My first commit for qprofile

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Soumyadip Ghosh",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Soumyadip Ghosh I'm a CSE Undergrad!",
+  githubUrl: "https://github.com/Binitagarwal",
+  linkedinUrl: "https://www.linkedin.com/in/binit-agarwal-447184178/",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
