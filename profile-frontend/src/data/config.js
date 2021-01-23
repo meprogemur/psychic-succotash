a8b56dacac20449dfde611ed853c82188169fcf3
Binit Agarwal <rockingbinitagarwal@gmail.com>
2021-01-23 23:32:19 +0530

    second commit

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "Soumyadip Ghosh",
   greeting: "Hey 👋",
   greetingDescription: "I'm Soumyadip Ghosh I'm a CSE Undergrad!",
-  githubUrl: "https://github.com/Binitagarwal",
-  linkedinUrl: "https://www.linkedin.com/in/binit-agarwal-447184178/",
+  githubUrl: "https://github.com/meprogemur",
+  linkedinUrl: "https://www.linkedin.com/in/soumyadip-ghosh-6050921aa/",
   cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
 };
